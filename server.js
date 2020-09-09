a1eb41c2c91e1ca840d7592fe925fc4bb6d43884
Lizzy <lizezzy@hotmail.com>
2020-09-09 10:57:16 +0300

    Added Supervillains

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+let supervillains = require ('supervillains');
+console.log(supervillains.random());
+
+supervillains.all.forEach(supervillain=> {
+    console.log(supervillain);
+});
+console.log(supervillains.all);
+
+
+
 let superheroes = require('superheroes');
 console.log(superheroes.random());
 
 superheroes.all.forEach(hero => {
     console.log(hero);
 });
-//console.log(superheroes.all);
+console.log(superheroes.all);
+
+
+
+console.log(`${superheroes.random()} fights ${supervillains.random()}`);
